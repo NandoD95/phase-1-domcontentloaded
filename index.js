457bee3e731e25fadeafec5b3b2881dcbe2789a0
Fernando De Jesus <fdejesus95@outlook.com>
2024-04-04 15:06:26 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    updateD();
+  });
+
+function updateD() {
+    document.getElementById("text")
+    .innerHTML = "This is really cool!"
+}
